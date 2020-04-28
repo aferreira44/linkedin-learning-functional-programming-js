f331160140910ce117544821ed4ad407d4c02faf
André Ferreira <contato@andreferreira.me>
2020-04-28 19:33:29 -0300

    Assigning functions to variables

--- Exercise_Files/02_02/Begin/02_debug.js
+++ Exercise_Files/02_02/Begin/02_debug.js
@@ -1,18 +1,34 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 02, Exercise 02
 
-function debug(message) {
-  console.log("DEBUG: " + message)
+const DEBUG_MODE_ENABLED = false;
+
+var debug;
+
+if (DEBUG_MODE_ENABLED) {
+  debug = printDebugMessage;
+} else {
+  debug = doNothing;
 }
 
-// ...
+function debug(message) {
+  console.log("DEBUG: " + message);
+}
 
-debug("Some debug message")
-
-// ...
-
-// function printDebugMessage(message) {
-//   console.log("DEBUG: " + message)
+// function debug(message) {
+//   if (DEBUG_MODE_ENABLED) {
+//     console.log("DEBUG: " + message);
+//   }
 // }
-//
-// function doNothing() { }
+
+// ...
+
+debug("Some debug message");
+
+// ...
+
+function printDebugMessage(message) {
+  console.log("DEBUG: " + message);
+}
+
+function doNothing() {}
