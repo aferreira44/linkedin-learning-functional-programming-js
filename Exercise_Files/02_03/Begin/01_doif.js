e6175764da2e4bee2573fbddd6c807845b3b2d51
André Ferreira <contato@andreferreira.me>
2020-04-28 19:55:13 -0300

    Passing functions as arguments

--- Exercise_Files/02_03/Begin/01_doif.js
+++ Exercise_Files/02_03/Begin/01_doif.js
@@ -1,22 +1,22 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 01
 
-const x = 1
-
-if (x === 1) {
-  sayXis1()
+function doIf(condition, func) {
+  if (condition) {
+    func();
+  }
 }
 
-if (x === "Bananas") {
-  sayXisBananas()
-}
+const x = 1;
 
-if (x < 10 && x > 0) {
-  sayXisBetween0And10()
-}
+doIf(x === 1, () => {
+  console.log("x is equal to 1");
+});
 
-function sayXis1() { console.log("x is equal to 1") }
+doIf(x === "Bananas", () => {
+  console.log("x is equal to 'Bananas'");
+});
 
-function sayXisBananas() { console.log("x is equal to 'Bananas'") }
-
-function sayXisBetween0And10() { console.log("x is between 0 and 10") }
+doIf(x < 10 && x > 0, () => {
+  console.log("x is between 0 and 10");
+});
--- Exercise_Files/02_03/Begin/03_ifElse.js
+++ Exercise_Files/02_03/Begin/03_ifElse.js
@@ -1,8 +1,22 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 03, Exercise 03
 
-function doIf(condition, func) {
+function ifElse(condition, func1, func2) {
   if (condition) {
-    func()
+    func1();
+  } else {
+    func2();
   }
 }
+
+var x = 1;
+
+ifElse(
+  x === 1,
+  () => {
+    console.log("x is 1");
+  },
+  () => {
+    console.log("x is not 1");
+  }
+);
