433419dcf9091c9b68272b6946ff738a8bdc20fa
André Ferreira <contato@andreferreira.me>
2020-04-28 20:48:09 -0300

    Closure and return functions

--- Exercise_Files/02_04/Begin/01_privateVars.js
+++ Exercise_Files/02_04/Begin/01_privateVars.js
@@ -1,23 +1,28 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 04, Exercise 01
 
-const myCounter = {
-  count: 0,
-  increment: function() {
-    this.count += 1
-  },
-  currentValue: function() {
-    return this.count
-  }
+function createCounter() {
+  var count = 0;
+
+  return {
+    increment: () => {
+      count += 1;
+    },
+    currentValue: () => {
+      return count;
+    },
+  };
 }
 
-console.log(myCounter.currentValue())
+var myCounter = createCounter();
 
-myCounter.increment()
-myCounter.increment()
+console.log(myCounter.currentValue());
 
-console.log(myCounter.currentValue())
+myCounter.increment();
+myCounter.increment();
 
-myCounter.count = 999 // uh oh!
+console.log(myCounter.currentValue());
 
-console.log(myCounter.currentValue())
+myCounter.count = 999; // uh oh!
+
+console.log(myCounter.currentValue());
--- Exercise_Files/02_04/Begin/02_capturingArguments.js
+++ Exercise_Files/02_04/Begin/02_capturingArguments.js
@@ -1,16 +1,20 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 04, Exercise 02
 
-function createCounter() {
-  var count = 0
-
+function createCounter(count) {
   return {
-    increment: function() {
-      count = count + 1
+    increment: function () {
+      count = count + 1;
     },
 
-    currentValue: function() {
-      return count
-    }
-  }
+    currentValue: function () {
+      return count;
+    },
+  };
 }
+
+var counterStartingAt5 = createCounter(5);
+
+var counterStartingAtMinus2 = createCounter(-2);
+
+counterStartingAt5.currentValue();
