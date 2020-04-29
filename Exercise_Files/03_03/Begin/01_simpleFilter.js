6fdb812933af729a3cef5d138c7e0ec3a4d7356b
André Ferreira <contato@andreferreira.me>
2020-04-29 00:14:36 -0300

    Filtering

--- Exercise_Files/03_03/Begin/01_simpleFilter.js
+++ Exercise_Files/03_03/Begin/01_simpleFilter.js
@@ -1,12 +1,10 @@
 // Learning Functional Programming with Javascript
 // Chapter 03, Video 03, Exercise 01
+const _ = require("lodash");
 
-var numbers = [ 1, 2, 3, 4, 5 ]
-var evensFromNumbers = [ ]
+const isEven = (x) => x % 2 === 0;
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i]
-  if (element % 2 === 0) {
-    evensFromNumbers.push(element)
-  }
-}
+const numbers = [1, 2, 3, 4, 5];
+const evensFromNumbers = _.filter(numbers, (el) => isEven(el));
+
+console.log(evensFromNumbers);
